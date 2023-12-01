c01f38eb17d832a7c1491972d09c1695af67204c
Kamil Kurach <kamilkurach@protonmail.com>
2023-12-01 06:25:55 +0100

    Add basic flask app

---
+++ main.py
@@ -0,0 +1,27 @@
+import webbrowser
+from flask import Flask, render_template
+from threading import Timer
+
+app = Flask(__name__)
+
+# parama
+command = None
+IP = None
+
+@app.route("/")
+def fela_screen():
+    return render_template("index.html")
+
+@app.route("/update")
+def update_params():
+    pass
+
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000/')
+
+def main():
+    Timer(1, open_browser).start()
+    app.run(port=5000)
+    
+if __name__== "__main__":
+    main()
