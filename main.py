7f1024523b6901e0fa2f89c93d9d95e2d438452c
Kamil Kurach <kamilkurach@protonmail.com>
2023-12-02 18:50:40 +0100

    Add button handler

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webbrowser
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from threading import Timer
+import subprocess
 
 app = Flask(__name__)
 
@@ -17,9 +18,24 @@ def fela_screen():
 def update_params():
     pass
 
+@app.route("/print", methods=['POST'])
+def test_print():
+    if request.method == 'POST':
+        try:
+            Timer(1, subprocess_command).start()
+            return render_template("index.html")
+        except Exception as e:
+            print("*** Message: subprocess error ***")
+            return redirect("/")
+        
+def subprocess_command():
+    print("*** Message: run subprocess command ***")
+    spo = subprocess.run(["pwd"], capture_output=True)  
+    print(spo.stdout)
+
 def open_browser():
     webbrowser.get(chrome_path).open(ip)
-    
+
 def main():
     Timer(1, open_browser).start()
     app.run(port=5000)
