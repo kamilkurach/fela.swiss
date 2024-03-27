ee76638c565493e0990671c4e5cdcb2500dbba91
kamilkurach <11406583+kamilkurach@users.noreply.github.com>
2024-03-28 00:13:10 +0100

    Develop (#3)
    * Wersja Demo do klienta
    
    * Update README.md
    
    * Update README.md
    
    * Downloaded from demo TVM.
    
    * Commit
    
    * clean up repo
    
    * Change folder name
    
    * Remove unused imports
    
    * Move command
    
    * Remove print
    
    * Fix dirs to scripts
    
    ---------
    
    Co-authored-by: auriadov <a@uwu.waw.pl>
    Co-authored-by: Tenji <36359708+valeowoia@users.noreply.github.com>

---
+++ gebe-imx/print.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/root/gebe-imx/print  /home/root/gebe-imx/print_6_inv.png
+/home/root/gebe-imx/print  /home/root/gebe-imx/print_14_inv.png
--- main.py
+++ main.py
@@ -7,22 +7,35 @@ app = Flask(__name__)
 
 # params
 command = []
+info_command = []
+ip_command = []
+
 try:
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             command.append(sys.argv[i])
     else:
-        command = ["pwd"]
+        command = ["gebe-imx/print.sh"]
+        info_command = ["scripts/render.sh"]
+        ip_command = ["scripts/ip.sh"]
 except IndexError:
-    command = ["pwd"]
+    info_command = ["scripts/render.sh"]
+    command = ["gebe-imx/print.sh"]
+    ip_command = ["skripts/ip.sh"]
 
 @app.route("/")
 def fela_screen():
     return render_template("index.html")
 
-@app.route("/info", methods=['POST'])
+@app.route("/render", methods=['POST'])
 def info_screen():
-    return redirect("/")
+    if request.method == 'POST':
+        try:
+            Timer(1, subprocess_info_command).start()
+            return redirect("/")
+        except Exception as e:
+            print(e)
+            return redirect("/")
 
 @app.route("/tickets", methods=['POST'])
 def test_tickets():
@@ -33,6 +46,30 @@ def test_tickets():
         except Exception as e:
             print("*** Message: subprocess error. Redirecting.. ***")
             return redirect("/")
+
+@app.route("/info", methods=['POST'])
+def info():
+    ip = get_ip()
+    print("test", ip)
+    if request.method == 'POST':
+        return render_template("info.html", value=ip)
+
+@app.route("/ip")
+def get_ip():
+    try:
+        spo = subprocess.run(ip_command, capture_output=True)
+        return str(spo.stdout.decode())
+    except Exception as e:
+        print(e)
+
+
+def subprocess_info_command():
+    print("*** Message: run subprocess command ***")
+    try:
+        spo = subprocess.run(info_command, capture_output=True)  
+        print(spo.stdout)
+    except Exception as e:
+        print(e)
         
 def subprocess_command():
     print("*** Message: run subprocess command ***")
@@ -46,7 +83,7 @@ def open_browser():
     # set default page to http://127.0.0.1:5000/
     # exit kiosk alt+F4
     try:
-        subprocess.run(["chromium-browser", "--kiosk", "--app=http://127.0.0.1:5000/"]) 
+        subprocess.run(["chromium", "0.0.0.0:5000"]) 
     except Exception as e:
         print(e) 
 
